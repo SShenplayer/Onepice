58e0957a65fce8ca0bb9f82edb59050289072702
SShenplayer <1196839549@qq.com>
2022-10-11 19:23:07 +0800

    define vuex store use API

---
+++ src/store/index.ts
@@ -0,0 +1,13 @@
+import { createStore } from 'vuex'
+export default createStore({
+    //数据仓库
+    state: {},
+    //获取数据仓库数据
+    getters: {},
+    //自定义操作数据仓库的接口
+    mutations: {},
+    //异步接口
+    actions: {},
+    //分包
+    modules: {},
+})
