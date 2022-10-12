363487f21baf0b11e7eff87e88966c83d82b809c
SShenplayer <1196839549@qq.com>
2022-10-12 11:22:23 +0800

    Add Vue.use component

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,11 +1,19 @@
 import { createStore } from 'vuex'
 export default createStore({
     //数据仓库
-    state: {},
+    state: {
+        Account: "",
+        Password: ""
+    },
     //获取数据仓库数据
     getters: {},
     //自定义操作数据仓库的接口
-    mutations: {},
+    mutations: {
+        ChangeLoginInfo(state, data) {
+            state.Account = data.Account
+            state.Password = data.Password
+        }
+    },
     //异步接口
     actions: {},
     //分包
